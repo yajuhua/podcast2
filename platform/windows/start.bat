4bf86e477c5857574f61051766a06e8852be9548
yajuhua <yajuhua@outlook.com>
2024-04-30 14:36:56 +0800

    用-classpath方式启动springboot

--- platform/windows/start.bat
+++ platform/windows/start.bat
@@ -1,9 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
 set PWD=%~dp0
 set JAVA=%PWD%jre8/bin
 set DATA=%PWD%data
 set "PATH=%JAVA%;%PWD%;%PATH%"
-java -version
+set "directory=%PWD%\lib"
+set "classpath=%PWD%;classes"
+
+rem 遍历指定目录下的所有文件
+for /r "%directory%" %%I in (*) do (
+    set "classpath=!classpath!;%%~fI"
+)
+
+rem 去掉开头的分号
+if defined classpath (
+    set "classpath=!classpath:~1!"
+)
 start /b %PWD%aria2c.exe --conf-path=%PWD%aria2.conf > aria2.log
 echo "aria2RPC start"
 chcp 65001
-java -jar -Dfile.encoding=utf-8 app.jar --podcast2.data.data-path=%DATA% 
+java -Dfile.encoding=utf-8 -classpath "%classpath%" -Dpodcast2.data.data-path=%DATA% io.github.yajuhua.podcast2.Podcast2Application
+endlocal
