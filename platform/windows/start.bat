e0afc86edd269c89438550ebfd9597905c02723e
yajuhua <yajuhua@outlook.com>
2024-04-30 16:24:20 +0800

    update start.bat

--- platform/windows/start.bat
+++ platform/windows/start.bat
@@ -5,15 +5,14 @@ set PWD=%~dp0
 set JAVA=%PWD%jre8/bin
 set DATA=%PWD%data
 set "PATH=%JAVA%;%PWD%;%PATH%"
-set "directory=%PWD%\lib"
+set "directory=%PWD%lib"
 set "classpath=%PWD%classes"
 
-rem 遍历指定目录下的所有文件
 for /r "%directory%" %%I in (*) do (
     set "classpath=!classpath!;%%~fI"
 )
 start /b %PWD%aria2c.exe --conf-path=%PWD%aria2.conf > aria2.log
 echo "aria2RPC start"
 chcp 65001
-java -Dfile.encoding=utf-8 -classpath "%classpath%" -Dpodcast2.data.data-path=%DATA% io.github.yajuhua.podcast2.Podcast2Application
+java -Dfile.encoding=utf-8 -classpath "!classpath!" -Dpodcast2.data.data-path=%DATA% io.github.yajuhua.podcast2.Podcast2Application
 endlocal
