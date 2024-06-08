22799458383e3b9a658e7cf9e8ee211132105817
yajuhua <yajuhua@outlook.com>
2024-06-08 16:22:19 +0800

    漏了type字段

--- src/main/java/io/github/yajuhua/podcast2/mapper/ItemsMapper.java
+++ src/main/java/io/github/yajuhua/podcast2/mapper/ItemsMapper.java
@@ -25,10 +25,10 @@ public interface ItemsMapper {
      * @param items
      */
     @Insert("INSERT INTO items(channel_uuid, uuid, title, image, description, link, enclosure, create_time, status" +
-            ", duration, downloader, file_name, format, download_progress, download_time_left, total_size, download_speed, operation, args,links) " +
+            ", duration, downloader, file_name, format, type, download_progress, download_time_left, total_size, download_speed, operation, args,links) " +
             "VALUES " +
             "(#{channelUuid}, #{uuid}, #{title}, #{image}, #{description}, #{link}, #{enclosure}, #{createTime}" +
-            ", #{status}, #{duration}, #{downloader}, #{fileName}, #{format}, #{downloadProgress}, #{downloadTimeLeft}" +
+            ", #{status}, #{duration}, #{downloader}, #{fileName}, #{format}, #{type}, #{downloadProgress}, #{downloadTimeLeft}" +
             ", #{totalSize}, #{downloadSpeed}, #{operation}, #{args}, #{links})")
     void insert(Items items);
 
