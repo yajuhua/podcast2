b7e15dc91b45ed2eb8b720d48755b0c603529bf9
yajuhua <yajuhua@outlook.com>
2024-06-15 14:32:57 +0800

    add alist config

---
+++ src/main/java/io/github/yajuhua/podcast2/config/AlistConfig.java
@@ -0,0 +1,42 @@
+package io.github.yajuhua.podcast2.config;
+
+import io.github.yajuhua.podcast2.alist.Alist;
+import io.github.yajuhua.podcast2.pojo.entity.AlistInfo;
+import io.github.yajuhua.podcast2.pojo.entity.ExtendInfo;
+import io.github.yajuhua.podcast2.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * alist配置
+ */
+@Configuration
+@Slf4j
+public class AlistConfig {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public Alist alist(){
+        Alist alist = new Alist();
+        try {
+            ExtendInfo extendInfo = userService.getExtendInfo();
+            if (extendInfo != null && extendInfo.getAlistInfo() != null){
+                AlistInfo alistInfo = extendInfo.getAlistInfo();
+                if (alistInfo.isOpen()){
+                    alist.setUrl(alistInfo.getUrl());
+                    alist.setUsername(alistInfo.getUsername());
+                    alist.setPassword(alistInfo.getPassword());
+                    alist.setPath(alistInfo.getPath());
+                    alist.refreshToken();
+                }
+            }
+        } catch (Exception e) {
+            log.error("alist配置错误：{}",e.getMessage());
+        }
+        return alist;
+    }
+}
