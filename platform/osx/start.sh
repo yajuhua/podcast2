d9d4285111ae5abe4c63d8c53cd9cc8782aff9a5
yajuhua <yajuhua@outlook.com>
2024-07-18 20:53:39 +0800

    [update]aria2 RPC启动交给程序内部

--- platform/osx/start.sh
+++ platform/osx/start.sh
@@ -11,6 +11,4 @@ for file in $(find "$directory" -type f); do
     classpath="$classpath:$file"
 done
 
-nohup "$PWD/aria2c" --conf-path="$PWD/aria2.conf" > aria2.log 2>&1 &
-echo "aria2RPC start"
 java -Dfile.encoding=utf-8 -classpath "$classpath" -Dpodcast2.data.data-path="$DATA" io.github.yajuhua.podcast2.Podcast2Application
