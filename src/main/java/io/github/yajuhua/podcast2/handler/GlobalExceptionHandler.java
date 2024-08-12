dae605e152930b4a57db6d146027597477bafe28
yajuhua <yajuhua@outlook.com>
2024-08-12 20:55:24 +0800

    [feat]拦截sqlite数据库SQLITE_BUSY异常情况

--- src/main/java/io/github/yajuhua/podcast2/handler/GlobalExceptionHandler.java
+++ src/main/java/io/github/yajuhua/podcast2/handler/GlobalExceptionHandler.java
@@ -4,11 +4,11 @@ import io.github.yajuhua.podcast2.common.exception.BaseException;
 import io.github.yajuhua.podcast2.common.exception.CertificateFileException;
 import io.github.yajuhua.podcast2.common.result.Result;
 import io.github.yajuhua.podcast2.mapper.UserMapper;
-import io.github.yajuhua.podcast2.pojo.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.sqlite.SQLiteException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -29,6 +29,20 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 拦截sqlite数据库异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result sQLiteExceptionHandler(SQLiteException ex){
+        if (ex.getMessage().contains("SQLITE_BUSY")){
+            return Result.error("数据库繁忙，请稍后再试。");
+        }else {
+            return Result.error(ex.getMessage());
+        }
     }
 
 
