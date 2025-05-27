346e5a796432809ea702842ab26a8ba4a5dba21a
yajuhua <89304018+yajuhua@users.noreply.github.com>
2025-05-27 23:25:22 +0800

    [update] -cp "classes;lib/*"

--- platform/windows/start.bat
+++ platform/windows/start.bat
@@ -5,13 +5,7 @@ set PWD=%~dp0
 set JAVA=%PWD%jre8/bin
 set DATA=%PWD%data
 set "PATH=%JAVA%;%PWD%;%PATH%"
-set "directory=%PWD%lib"
-set "classpath=%PWD%classes"
-
-for /r "%directory%" %%I in (*) do (
-    set "classpath=!classpath!;%%~fI"
-)
 
 chcp 65001
-java -Dfile.encoding=utf-8 -classpath "!classpath!" -Dpodcast2.data.data-path=%DATA% io.github.yajuhua.podcast2.Podcast2Application
+java -Dfile.encoding=utf-8 -cp "classes;lib/*" -Dpodcast2.data.data-path=%DATA% io.github.yajuhua.podcast2.Podcast2Application
 endlocal
