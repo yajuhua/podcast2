51108658bf671a4e50d52ef2038f853c4aea4636
yajuhua <yajuhua@outlook.com>
2025-07-18 00:28:54 +0800

    为了方便前端开发，将控制类接口添加`/api`前缀，但排除xml、groupXml和index接口

--- src/main/java/io/github/yajuhua/podcast2/controller/ResourcesController.java
+++ src/main/java/io/github/yajuhua/podcast2/controller/ResourcesController.java
@@ -23,7 +23,7 @@ public class ResourcesController {
      * @return
      */
     @ApiOperation("解析alist资源")
-    @GetMapping("/resources/alist/{fileName}")
+    @GetMapping("/api/resources/alist/{fileName}")
     public RedirectView parseAlistResources(@PathVariable String fileName) {
         RedirectView redirectView = new RedirectView();
         String fileUrl = alist.getFileUrl(fileName);
