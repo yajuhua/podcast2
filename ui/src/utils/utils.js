fbdad9105a4dc858519072e72e43a00eee2ba658
yajuhua <yajuhua@outlook.com>
2025-09-01 19:47:52 +0800

    fix 使用使用 Clipboard API拷贝

--- ui/src/utils/utils.js
+++ ui/src/utils/utils.js
@@ -1,6 +1,27 @@
 // 工具库写法
-export function copy(content) {
-    return navigator.clipboard.writeText(content)
-      .then(() => true)
-      .catch(() => false);
+export async function copy(content) {
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    try {
+      await navigator.clipboard.writeText(content);
+      return true;
+    } catch (e) {
+      console.error("Clipboard API failed", e);
+      return fallbackCopy(content);
+    }
+  } else {
+    return fallbackCopy(content);
   }
+}
+
+function fallbackCopy(content) {
+  const input = document.createElement('textarea');
+  input.value = content;
+  input.setAttribute('readonly', '');
+  input.style.position = 'absolute';
+  input.style.left = '-9999px';
+  document.body.appendChild(input);
+  input.select();
+  const result = document.execCommand('copy');
+  document.body.removeChild(input);
+  return result;
+}
