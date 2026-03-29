3eaadaf091be8e87e0af42a244cf69a7b09030d0
yajuhua <yajuhua@outlook.com>
2026-03-29 22:19:07 +0800

    向前端日志ws发送心跳包

--- src/main/java/io/github/yajuhua/podcast2/handler/ws/LogsWebSocketHandler.java
+++ src/main/java/io/github/yajuhua/podcast2/handler/ws/LogsWebSocketHandler.java
@@ -11,6 +11,10 @@ import org.springframework.web.socket.*;
 import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import static io.github.yajuhua.podcast2.interceptor.WSTokenInterceptor.sessionMap;
 
@@ -20,6 +24,8 @@ public class LogsWebSocketHandler implements WebSocketHandler {
 
     private static DataPathProperties dataPathProperties;
     private Tailer tailer;
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledFuture<?> heartbeatTask;
 
     @Autowired
     public void setDataPathProperties(DataPathProperties dataPathProperties) {
@@ -30,6 +36,7 @@ public class LogsWebSocketHandler implements WebSocketHandler {
         LogsListener logsListener = new LogsListener(session);
         tailer = Tailer.create(new File(dataPathProperties.getInfoPath()), logsListener, 1000, true);
         log.info("客户端：{} 建立连接", session.getUri().getPath());
+        startHeartbeat(session);
         sessionMap.put(session.getId(), session);
     }
 
@@ -59,10 +66,37 @@ public class LogsWebSocketHandler implements WebSocketHandler {
         log.info("客户端：{} 连接断开", session.getUri().getPath());
         tailer.close();
         sessionMap.remove(session.getId());
+        stopHeartbeat();
     }
 
     @Override
     public boolean supportsPartialMessages() {
         return false;
+    }
+
+    /**
+     * 启动心跳任务
+     * 每隔25秒向服务器发送一个Ping帧
+     */
+    private void startHeartbeat(WebSocketSession session) {
+        heartbeatTask = scheduler.scheduleAtFixedRate(() -> {
+            try {
+                if (session != null && session.isOpen()) {
+                    session.sendMessage(new PingMessage());
+                    log.debug("发送Ping心跳...");
+                }
+            } catch (Exception e) {
+                log.error("发送Ping心跳失败", e);
+            }
+        }, 25, 25, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 停止心跳任务
+     */
+    private void stopHeartbeat() {
+        if (heartbeatTask != null && !heartbeatTask.isDone()) {
+            heartbeatTask.cancel(true);
+        }
     }
 }
