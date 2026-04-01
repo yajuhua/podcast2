45a2d78e898ff31f47cae54b3af3bd526720306a
yajuhua <yajuhua@outlook.com>
2026-04-02 00:20:39 +0800

    当quartz无法转换成Unix Cron时，将使用默认Unix Cron表达式 `String defaultCron = "0 0 12 * * ? *";`

--- src/main/java/io/github/yajuhua/podcast2/common/utils/CronUtils.java
+++ src/main/java/io/github/yajuhua/podcast2/common/utils/CronUtils.java
@@ -5,7 +5,9 @@ import com.cronutils.model.Cron;
 import com.cronutils.model.CronType;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.parser.CronParser;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CronUtils {
     /**
      * Quartz转换成UNIX
@@ -13,9 +15,16 @@ public class CronUtils {
      * @return
      */
     public static String fromQuartzToUnix(String quartz){
-        CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ));
-        CronMapper cronMapper = CronMapper.fromQuartzToUnix();
-        Cron cron4jCron = cronMapper.map(parser.parse(quartz));
-        return cron4jCron.asString();
+        try {
+            CronParser parser = new CronParser(CronDefinitionBuilder.instanceDefinitionFor(CronType.QUARTZ));
+            CronMapper cronMapper = CronMapper.fromQuartzToUnix();
+            Cron cron4jCron = cronMapper.map(parser.parse(quartz));
+            return cron4jCron.asString();
+        } catch (Exception e) {
+            String defaultCron = "0 12 * * *"; //每天12点
+            log.error("{}  Cron表达式解析失败: {}  使用默认表达式: {}", quartz ,e.getMessage(), defaultCron);
+            log.warn("仅支持Unix Cron");
+            return defaultCron;
+        }
     }
 }
