66b59be3d17d48fddcb4a53a2fad6f99bbf22c33
yajuhua <yajuhua@outlook.com>
2026-04-24 22:05:12 +0800

    highlight.js仅使用bash和Json

--- ui/babel.config.js
+++ ui/babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  plugins: [
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk"
+      }
+    ]
   ]
-}
+};
